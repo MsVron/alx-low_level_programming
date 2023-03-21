fddecaf2a9a86556ae392d841cb8d904fbeeab9b
Aya Berroukech <ayagym14@gmail.com>
2023-03-21 15:32:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+utable File  21 lines (18 sloc)  267 Bytes
+
+#include "main.h"
+
+/**
+ * main - print Holberton, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
