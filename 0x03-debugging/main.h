aff00d082fb1f863c9b25965bd4a58037ba9b5bc
Aya Berroukech <ayagym14@gmail.com>
2023-03-23 14:59:28 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,35 @@
+
+#ifndef HOLBERTON_H_SEEN
+#define HOLBERTON_H_SEEN
 #include <stdio.h>
-#include "main.h"
+
+/**
+ * positive_or_negative - checkes wheteher a number is positive or negative
+ * @i: the number to be checked
+ * Return: 0, 1, and -1 according to the sign
+ */
+
+void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
+
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day);
 
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -10,34 +40,6 @@
  * Return: void
  */
 
-void print_remaining_days(int month, int day, int year)
-{
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-	{
+void print_remaining_days(int month, int day, int year);
 
-		if (month > 2)
-		{
-			day++;
-		}
-
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-
-	}
-
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month,
-			       day - 31, year);
-		}
-
-		else
-		{
-
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
-}
+#endif
