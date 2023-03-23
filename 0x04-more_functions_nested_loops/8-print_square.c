d62a5743fb735aa041061c427332dad56e9adb59
Aya Berroukech <ayagym14@gmail.com>
2023-03-23 16:48:15 -0700

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+ecutable File  21 lines (18 sloc)  273 Bytes
+
+#include "main.h"
+
+/**
+ * print_square - prints a diagonal.
+ * @n : the length of squares
+ * Return: 0
+ */
+
+void print_square(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
