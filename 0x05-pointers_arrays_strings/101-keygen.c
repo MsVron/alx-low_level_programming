fd40c32aec660728ec422a730c3ba07b71425d2c
Aya Berroukech <ayagym14@gmail.com>
2023-03-27 20:09:15 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,22 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - generates random passwords for 101-crackme
- * Return: zero
- */
+#define PASSWORD_LEN 8
+
 int main(void)
 {
-	int sum;
-	char c;
+    srand(time(NULL));  // Initialize the random number generator with the current time
 
-	srand(time(NULL));
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
-	return (0);
+    char password[PASSWORD_LEN + 1];  // Allocate space for the password (plus a null terminator)
+
+    for (int i = 0; i < PASSWORD_LEN; i++) {
+        int r = rand() % 62;  // Generate a random number from 0 to 61
+        if (r < 26) {
+            password[i] = 'A' + r;  // If the number is less than 26, use a capital letter
+        } else if (r < 52) {
+            password[i] = 'a' + r - 26;  // If the number is between 26 and 51, use a lowercase letter
+        } else {
+            password[i] = '0' + r - 52;  // If the number is between 52 and 61, use a digit
+        }
+    }
+
+    password[PASSWORD_LEN] = '\0';  // Add a null terminator to the end of the password
+
+    printf("%s\n", password);  // Print the password
+
+    return 0;
 }
+
