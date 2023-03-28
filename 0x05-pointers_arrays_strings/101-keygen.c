7dfd819824f1cc3651ebc96077a0424c29dad047
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-28 03:14:05 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LEN 8
-
+/**
+ * main - generates random passwords for 101-crackme
+ * Return: zero
+ */
 int main(void)
 {
-    srand(time(NULL));  // Initialize the random number generator with the current time
+    int sum = 0;
+    char c;
 
-    char password[PASSWORD_LEN + 1];  // Allocate space for the password (plus a null terminator)
+    srand(time(NULL));
 
-    for (int i = 0; i < PASSWORD_LEN; i++) {
-        int r = rand() % 62;  // Generate a random number from 0 to 61
-        if (r < 26) {
-            password[i] = 'A' + r;  // If the number is less than 26, use a capital letter
-        } else if (r < 52) {
-            password[i] = 'a' + r - 26;  // If the number is between 26 and 51, use a lowercase letter
-        } else {
-            password[i] = '0' + r - 52;  // If the number is between 52 and 61, use a digit
-        }
+    while (sum <= 2645)
+    {
+        c = rand() % 128;
+        sum += c;
+        putchar(c);
     }
 
-    password[PASSWORD_LEN] = '\0';  // Add a null terminator to the end of the password
+    putchar(2772 - sum);
 
-    printf("%s\n", password);  // Print the password
+    putchar('\n');
 
-    return 0;
+    return (0);
 }
