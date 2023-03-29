db83cc301567118aff18d41008896b624a891782
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-29 21:24:53 +0000

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
 #include <ctype.h>
 #include <string.h>
 
-/
+/**
  * _isdigit - checks if a character is a digit
  * @c: character to check
  *
@@ -14,10 +14,10 @@
  */
 int _isdigit(int c)
 {
-  return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
 
-/
+/**
  * _atoi - convert a string to an integer
  * @s: string to convert
  *
@@ -25,26 +25,26 @@ int _isdigit(int c)
  */
 int _atoi(char *s)
 {
-  int res = 0;
-  int sign = 1;
-  int i = 0;
+	int res = 0;
+	int sign = 1;
+	int i = 0;
 
-  while (s[i] != '\0')
-  {
-    if (s[i] == '-')
-    {
-      sign *= -1;
-    }
-    else if (_isdigit(s[i]))
-    {
-      res = res * 10 + (s[i] - '0');
-    }
-    else if (res > 0)
-    {
-      break;
-    }
-    i++;
-  }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (_isdigit(s[i]))
+		{
+			res = res * 10 + (s[i] - '0');
+		}
+		else if (res > 0)
+		{
+			break;
+		}
+		i++;
+	}
 
-  return (sign * res);
+	return (sign * res);
 }
