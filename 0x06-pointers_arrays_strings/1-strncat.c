3f8eb716885e1251cc4612256ac4740acff5e6b4
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-29 22:15:55 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,23 @@
 /**
- * _strncat - concatenate two strings, limiting the number of bytes to copy.
+ * _strncat - concatenates two strings, taking at most n bytes from src
  *
  * @dest: pointer to the destination string
  * @src: pointer to the source string
- * @n: maximum number of bytes to concatenate
+ * @n: the maximum number of bytes to concatenate from src
  *
  * Return: pointer to the resulting string @dest
  */
 char *_strncat(char *dest, const char *src, size_t n)
 {
-    size_t dest_len = 0;
+    size_t dest_len = strlen(dest);
+    size_t i;
     
-    // Find the end of dest string
-    while (dest[dest_len] != '\0')
-        dest_len++;
-
-    // Append at most n bytes from src to dest
-    for (size_t i = 0; i < n && src[i] != '\0'; i++)
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
         dest[dest_len + i] = src[i];
-
-    // Add a terminating null byte to the end of dest
-    dest[dest_len + n] = '\0';
-
+    }
+    
+    dest[dest_len + i] = '\0';
+    
     return dest;
 }
