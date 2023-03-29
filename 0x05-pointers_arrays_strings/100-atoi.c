2b46dc1d758f06e8f26298a23cd40f1c34026d18
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-29 20:58:00 +0000

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,54 +6,40 @@
 #include <ctype.h>
 #include <string.h>
 
-/**
- * _isdigit - checks if a character is a digit (0 through 9).
- *
- * @c: character to check
- *
- * Return: 1 if c is a digit, 0 otherwise.
- */
 int _isdigit(char c)
 {
-	return (c >= '0' && c <= '9');
+    return (c >= '0' && c <= '9');
 }
 
-/**
- * _atoi - converts a string to an integer.
- *
- * @s: pointer to the string to convert
- *
- * Return: the converted integer.
- */
 int _atoi(char *s)
 {
-	int i, sign, num;
+    int i, sign, num;
 
-	i = 0;
-	sign = 1;
-	num = 0;
+    i = 0;
+    sign = 1;
+    num = 0;
 
-	while (s[i] == ' ')
-		i++;
+    while (s[i] == ' ')
+        i++;
 
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
+    if (s[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
 
-	for (; _isdigit(s[i]); i++)
-	{
-		if (num > INT_MAX / 10 || (num == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
-		{
-			if (sign == 1)
-				return INT_MAX;
-			else
-				return INT_MIN;
-		}
+    for (; _isdigit(s[i]); i++)
+    {
+        if (num > INT_MAX / 10 || (num == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+        {
+            if (sign == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
 
-		num = (num * 10) + (s[i] - '0');
-	}
+        num = (num * 10) + (s[i] - '0');
+    }
 
-	return (num * sign);
+    return (num * sign);
 }
