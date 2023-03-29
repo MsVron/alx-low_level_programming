db459f93718017cccaf8566c0b6efacd66c9dad8
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-29 20:44:11 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
 
 /**
  * _atoi - convert a string to an integer.
@@ -10,36 +9,49 @@
  *
  * Return: the integer value of the string.
  */
-
 int _atoi(char *s)
 {
-	int sign = 1, value = 0;
-	char *p = s;
-
-	/* skip leading whitespace */
-	while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r' || *p == '\f' || *p == '\v')
-	{
-		p++;
-	}
-
-	/* handle sign */
-	if (*p == '-')
-	{
-		sign = -1;
-		p++;
-	}
-	else if (*p == '+')
-	{
-		sign = 1;
-		p++;
-	}
-
-	/* process digits */
-	while (*p >= '0' && *p <= '9')
-	{
-		value = value * 10 + (*p - '0');
-		p++;
-	}
-
-	return sign * value;
+    int sign = 1, value = 0, max_int = INT_MAX / 10, min_int = INT_MIN / 10;
+    char *p = s;
+    
+    /* skip leading whitespace */
+    while (isspace(*p))
+    {
+        p++;
+    }
+    
+    /* handle sign */
+    if (*p == '-')
+    {
+        sign = -1;
+        p++;
+    }
+    else if (*p == '+')
+    {
+        sign = 1;
+        p++;
+    }
+    
+    /* process digits */
+    while (isdigit(*p))
+    {
+        int digit = *p - '0';
+        
+        /* check for integer overflow */
+        if (value > max_int || (value == max_int && digit > 7))
+        {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        
+        /* check for integer underflow */
+        if (value < min_int || (value == min_int && digit > 8))
+        {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        
+        value = value * 10 + digit;
+        p++;
+    }
+    
+    return sign * value;
 }
