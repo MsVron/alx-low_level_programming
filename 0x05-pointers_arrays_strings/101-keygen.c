c9a2d722f34f75ad6c72b77d82103f8513680f1d
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-29 20:50:41 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,10 @@
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include <limits.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
  * _atoi - convert a string to an integer.
@@ -13,13 +17,13 @@ int _atoi(char *s)
 {
     int sign = 1, value = 0, max_int = INT_MAX / 10, min_int = INT_MIN / 10;
     char *p = s;
-    
+
     /* skip leading whitespace */
     while (isspace(*p))
     {
         p++;
     }
-    
+
     /* handle sign */
     if (*p == '-')
     {
@@ -31,27 +35,27 @@ int _atoi(char *s)
         sign = 1;
         p++;
     }
-    
+
     /* process digits */
     while (isdigit(*p))
     {
         int digit = *p - '0';
-        
+
         /* check for integer overflow */
         if (value > max_int || (value == max_int && digit > 7))
         {
             return (sign == 1) ? INT_MAX : INT_MIN;
         }
-        
+
         /* check for integer underflow */
         if (value < min_int || (value == min_int && digit > 8))
         {
             return (sign == 1) ? INT_MAX : INT_MIN;
         }
-        
+
         value = value * 10 + digit;
         p++;
     }
-    
+
     return sign * value;
 }
