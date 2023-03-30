533fc12358004afe06a90f0e797cf0e3cc6ca872
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-30 23:25:22 +0000

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,34 +1,30 @@
+#include "main.h"
+
 /**
- * leet - Encodes a string into 1337.
- *
- * @str: The string to be encoded.
- *
- * Return: The resulting encoded string.
+ * leet - encode string into 1337 leet
+ * @s: string to manipulate
+ * Return: string
  */
-char *leet(char *str)
+
+char *leet(char *s)
 {
-    int i, j;
-    char *leet_str = str;
+	char alpha[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char num[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int i = 0, j = 0;
 
-    /* Encoding rules */
-    char letters[] = "aAeEoOtTlL";
-    char numbers[] = "44337011";
-
-    /* Loop through each character of the string */
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        /* Loop through each letter of the encoding rules */
-        for (j = 0; letters[j] != '\0'; j++)
-        {
-            /* If the current character matches a letter from the encoding rules */
-            if (str[i] == letters[j])
-            {
-                /* Replace the letter with the corresponding number */
-                leet_str[i] = numbers[j];
-                break;
-            }
-        }
-    }
-
-    return leet_str;
+	while (*(s + i) != '\0')
+	{
+		while (j < 10)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = num[j];
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (s);
 }
