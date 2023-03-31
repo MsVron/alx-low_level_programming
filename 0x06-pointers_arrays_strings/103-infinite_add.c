128d809b1099d41eb177e0983fa5e15f1ef4e6db
MsVron <126300537+MsVron@users.noreply.github.com>
2023-03-31 00:01:24 +0000

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,60 @@
+#include "main.h"
+
 /**
- * infinite_add - Adds two numbers
- * @n1: The first number to add
- * @n2: The second number to add
- * @r: The buffer to store the result
- * @size_r: The size of the buffer
+ * infinite_add - Adds two numbers stored as strings
+ * @n1: First number
+ * @n2: Second number
+ * @r: Buffer to store the result
+ * @size_r: Size of the buffer
  *
- * Return: A pointer to the result, or 0 if the buffer is too small
+ * Return: Pointer to the buffer, or 0 if the buffer is too small
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i = 0, j = 0, k = 0, carry = 0, sum = 0, len1 = 0, len2 = 0;
+	int l1 = 0, l2 = 0, big, add, re = 0, n1_int, n2_int;
 
-    while (*(n1 + len1))
-        len1++;
-    while (*(n2 + len2))
-        len2++;
+	/* Find length of the strings */
+	while (*(n1 + l1))
+		l1++;
+	while (*(n2 + l2))
+		l2++;
 
-    if (len1 + 1 > size_r || len2 + 1 > size_r)
-        return (0);
+	/* Check if the buffer is large enough */
+	if (l1 >= size_r || l2 >= size_r || size_r <= 0)
+		return (0);
 
-    len1--;
-    len2--;
+	/* Initialize variables */
+	big = l1 >= l2 ? l1 : l2;
+	*(r + big + 1) = '\0';
+	l1--, l2--, size_r--;
 
-    while (len1 >= 0 || len2 >= 0)
-    {
-        sum = carry;
-        if (len1 >= 0)
-            sum += *(n1 + len1--) - '0';
-        if (len2 >= 0)
-            sum += *(n2 + len2--) - '0';
+	/* Add digits from right to left */
+	while (big >= 0)
+	{
+		n1_int = l1 >= 0 ? *(n1 + l1--) - '0' : 0;
+		n2_int = l2 >= 0 ? *(n2 + l2--) - '0' : 0;
+		add = n1_int + n2_int + re;
+		if (add >= 10)
+			re = 1;
+		else
+			re = 0;
+		*(r + big--) = add % 10 + '0';
+		size_r--;
+		if (size_r < 0)
+			return (0);
+	}
 
-        *(r + k++) = sum % 10 + '0';
-        carry = sum / 10;
-    }
+	/* Add carry digit if necessary */
+	if (re)
+	{
+		if (size_r < 1)
+			return (0);
+		*(r + 0) = re + '0';
+	}
+	else
+	{
+		r++;
+	}
 
-    if (carry)
-        *(r + k++) = carry + '0';
-
-    if (k > size_r)
-        return (0);
-
-    *(r + k) = '\0';
-
-    i = 0;
-    j = k - 1;
-
-    while (i < j)
-    {
-        *(r + i) ^= *(r + j);
-        *(r + j) ^= *(r + i);
-        *(r + i) ^= *(r + j);
-
-        i++;
-        j--;
-    }
-
-    return (r);
+	return (r);
 }
