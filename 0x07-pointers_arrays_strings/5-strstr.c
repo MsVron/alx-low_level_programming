863581c096ad1eee46dd71fa8d16d39b6d8ba75e
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-04 01:54:44 +0000

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,20 +1,29 @@
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to search in
+ * @needle: the substring to search for
+ *
+ * Return: pointer to the beginning of the located substring, or NULL if not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
     if (*needle == '\0')
-        return haystack;
+        return (haystack);
 
-    for (; *haystack != '\0'; haystack++) {
+    for (; *haystack != '\0'; haystack++)
+    {
         char *h = haystack;
         char *n = needle;
 
-        while (*n != '\0' && *h == *n) {
+        while (*n != '\0' && *h == *n)
+        {
             h++;
             n++;
         }
 
         if (*n == '\0')
-            return haystack;
+            return (haystack);
     }
 
-    return NULL;
+    return (NULL);
 }
