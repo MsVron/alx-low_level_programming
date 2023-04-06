042778d993195118fac4c15f0e01feb7d213d64e
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-06 00:15:57 +0000

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * _sqrt_recursion - Computes the natural square root of a number recursively
@@ -17,4 +16,29 @@ int _sqrt_recursion(int n)
 		return (n);
 
 	return (_sqrt_helper(n, 1, n)); /* Call helper function with lower bound = 1 and upper bound = n */
+}
+
+/**
+ * _sqrt_helper - Helper function for _sqrt_recursion
+ * @n: The number whose square root is to be computed
+ * @low: The lower bound of the square root
+ * @high: The upper bound of the square root
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural square root
+ */
+
+int _sqrt_helper(int n, int low, int high)
+{
+	int mid = (low + high) / 2;
+
+	if (mid * mid == n) /* Base case */
+		return (mid);
+
+	if (low >= high) /* Error case */
+		return (-1);
+
+	if (mid * mid > n) /* Recursive case - search the lower half */
+		return (_sqrt_helper(n, low, mid - 1));
+	else /* Recursive case - search the upper half */
+		return (_sqrt_helper(n, mid + 1, high));
 }
