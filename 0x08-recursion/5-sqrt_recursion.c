88533151984f77ac2d35979e2875f16cb5a5a32b
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-06 00:28:59 +0000

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,16 +10,16 @@
  */
 int _sqrt_helper(int n, int i)
 {
-    /* Base case */
-    if (i * i == n)
-        return i;
+        /* Base case */
+        if (i * i == n)
+                return (i);
 
-    /* Terminating case */
-    if (i * i > n)
-        return -1;
+        /* Terminating case */
+        if (i * i > n)
+                return (-1);
 
-    /* Recursive case */
-    return _sqrt_helper(n, i + 1);
+        /* Recursive case */
+        return (_sqrt_helper(n, i + 1));
 }
 
 /**
@@ -30,5 +30,5 @@ int _sqrt_helper(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    return _sqrt_helper(n, 1);
+        return (_sqrt_helper(n, 1));
 }
