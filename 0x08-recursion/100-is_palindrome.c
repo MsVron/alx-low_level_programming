18703c2e19a79b2e4ca84de8e1ebaa4f67a2dd76
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-06 00:48:34 +0000

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,18 +11,25 @@ int is_palindrome(char *s)
 {
     int len = 0;
 
-    /* Get the length of the string */
-    while (*(s + len) != '\0')
-        len++;
+    /* Helper function to get the length of the string */
+    int get_length(char *s, int len)
+    {
+        if (*s == '\0')
+            return len;
+        else
+            return get_length(s + 1, len + 1);
+    }
+
+    len = get_length(s, 0);
 
     /* Base case: a string of length 0 or 1 is a palindrome */
     if (len <= 1)
-        return (1);
+        return 1;
 
     /* Recursive case: compare the first and last characters */
     if (*s == *(s + len - 1))
-        return (is_palindrome(s + 1) && is_palindrome(s + len - 1 - 1));
+        return is_palindrome(s + 1) && is_palindrome(s + len - 1 - 1);
 
     /* If the first and last characters are different, it's not a palindrome */
-    return (0);
+    return 0;
 }
