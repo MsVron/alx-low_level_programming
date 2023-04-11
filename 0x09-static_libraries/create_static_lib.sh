78951cf5c7959e9533a00b90e2f7ad4ab148597c
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-11 00:53:48 +0000

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-gcc -c -Wall -Werror -Wextra *.c
+
+# compile all .c files into .o files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# create the static library from the .o files
 ar -rc liball.a *.o
+
+# index the static library
+ranlib liball.a
+
+# clean up the .o files
+rm *.o
