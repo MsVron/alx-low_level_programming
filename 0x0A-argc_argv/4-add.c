bbe8fa80bad8c10d049c45df38257cce91801014
Aya Berroukech <ayagym14@gmail.com>
2023-04-10 18:39:40 -0700

    ++ numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of all the arguments passed to the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, res = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			res += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", res);
+	return (0);
+}
