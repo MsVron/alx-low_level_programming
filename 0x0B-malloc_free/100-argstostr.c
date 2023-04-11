cfc2900d70f410d3b9ef6759b2af2d18a46cbfb2
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-11 23:58:44 +0000

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,36 +12,72 @@
  */
 char *argstostr(int ac, char **av)
 {
-        char *str;
-        int i, j, k = 0, len = 0;
+	char *str;
+	int i, j, k = 0, len = 0;
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                        len++;
-                len++;
-        }
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
 
-        str = malloc(sizeof(char) * (len + 1));
+	str = malloc(sizeof(char) * (len + 1));
 
-        if (str == NULL)
-                return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j]; j++)
-                {
-                        str[k] = av[i][j];
-                        k++;
-                }
-                str[k] = '\n';
-                k++;
-        }
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
 
-        str[k] = '\0';
+	str[k] = '\0';
 
-        return (str);
+	return (str);
+}
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, length;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	/*find length of vector + '\0' which makes it a 2d array*/
+	length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
+	}
+
+	str = malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
 }
