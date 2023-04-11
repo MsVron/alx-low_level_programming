22cabeb9ee27c997b0c2500807f2111262134400
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-11 23:22:59 +0000

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,27 +16,27 @@ char *str_concat(char *s1, char *s2)
 
     if (s1 == NULL)
         s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[s1_len])
-        s1_len++;
+	while (s1[s1_len])
+		s1_len++;
 
-    while (s2[s2_len])
-        s2_len++;
+	while (s2[s2_len])
+		s2_len++;
 
-    concat_str = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	concat_str = malloc(sizeof(char) * (s1_len + s2_len + 1));
 
-    if (concat_str == NULL)
-        return (NULL);
+	if (concat_str == NULL)
+		return (NULL);
 
-    for (i = 0; s1[i]; i++)
-        concat_str[i] = s1[i];
+	for (i = 0; s1[i]; i++)
+		concat_str[i] = s1[i];
 
-    for (j = 0; s2[j]; j++)
-        concat_str[i + j] = s2[j];
+	for (j = 0; s2[j]; j++)
+		concat_str[i + j] = s2[j];
 
-    concat_str[i + j] = '\0';
+	concat_str[i + j] = '\0';
 
-    return (concat_str);
+	return (concat_str);
 }
