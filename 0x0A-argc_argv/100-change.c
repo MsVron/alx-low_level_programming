319b1a4089b4374ddc73222aaea517a1341ab126
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-11 07:18:40 +0000

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - prints the minimum number of coins to make change for an amount of money
+ * main - prints its name followed by a new line
  * @argc: argument count
  * @argv: argument vector
- *
- * Return: 0 on success, 1 on failure
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int coins = 0, cents;
+	int coins = 0, change;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
+	else if(atoi(argv[1]) < 0)
+	{
 		printf("0\n");
+		return (0);
+	}
 	else
 	{
-		while (cents >= 25)
+		change = atoi(argv[1]);
+		while (change != 0)
 		{
-			cents -= 25;
-			coins++;
+			if (change >= 25)
+			{
+				coins = coins + (change / 25);
+				change = change % 25;
+			}
+			else if (change >= 10)
+			{
+				coins = coins + (change / 10);
+				change = change % 10;
+			}
+			else if (change >= 5)
+                        {
+                                coins = coins + (change / 5);
+                                change = change % 5;
+                        }
+			else if (change >= 2)
+                        {
+                                coins = coins + (change / 2);
+                                change = change % 2;
+                        }
+			else if (change >= 1)
+                        {
+                                coins = coins + (change / 1);
+                                change = change % 1;
+                        }
 		}
-		while (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		while (cents >= 5)
-		{
-			cents -= 5;
-			coins++;
-		}
-		while (cents >= 2)
-		{
-			cents -= 2;
-			coins++;
-		}
-		while (cents >= 1)
-		{
-			cents -= 1;
-			coins++;
-		}
-		printf("%d\n", coins);
 	}
+	printf("%d\n", coins);
 	return (0);
 }
