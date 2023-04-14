e52fef84eadfed8205f05db2f9d1d7cf64268cee
Aya Berroukech <ayagym14@gmail.com>
2023-04-13 18:10:10 -0700

    string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ * @n: number of bytes to concatenate from s2
+ * Return: pointer to the newly allocated space in memory
+ */
+
+#include "holberton.h"
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+	if (n >= len2)
+		n = len2;
+	str = malloc(sizeof(char) * (len1 + n + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		str[i] = s2[j];
+	str[i] = '\0';
+	return (str);
+}
