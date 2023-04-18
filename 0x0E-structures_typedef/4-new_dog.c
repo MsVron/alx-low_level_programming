4ba98640171236417e091ea38788a6f9ee286645
Aya Berroukech <ayagym14@gmail.com>
2023-04-17 18:58:46 -0700

    new doggo

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,63 +1,52 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * new_dog - create a new dog
- *
+ * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog
+ * Return: pointer to the new dog or NULL if fails
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	char *new_name, *new_owner;
-	int name_len, owner_len, i;
+	dog_t *new_dog;
+	char *copy_name, *copy_owner;
+	unsigned int i, name_len, owner_len;
 
-	/* Check if name and owner are not NULL */
-	if (name == NULL || owner == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 		return (NULL);
 
-	/* Allocate memory for dog */
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-
-	/* Get the length of name and owner */
-	for (name_len = 0; name[name_len]; name_len++)
+	for (name_len = 0; name[name_len] != '\0'; name_len++)
 		;
-	for (owner_len = 0; owner[owner_len]; owner_len++)
+	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
 		;
 
-	/* Allocate memory for new_name and new_owner */
-	new_name = malloc(sizeof(char) * (name_len + 1));
-	if (new_name == NULL)
+	copy_name = malloc(sizeof(char) * (name_len + 1));
+	if (copy_name == NULL)
 	{
-		free(dog);
+		free(new_dog);
 		return (NULL);
 	}
-	new_owner = malloc(sizeof(char) * (owner_len + 1));
-	if (new_owner == NULL)
-	{
-		free(new_name);
-		free(dog);
-		return (NULL);
-	}
-
-	/* Copy name and owner to new_name and new_owner */
 	for (i = 0; i <= name_len; i++)
-		new_name[i] = name[i];
+		copy_name[i] = name[i];
+
+	copy_owner = malloc(sizeof(char) * (owner_len + 1));
+	if (copy_owner == NULL)
+	{
+		free(new_dog);
+		free(copy_name);
+		return (NULL);
+	}
 	for (i = 0; i <= owner_len; i++)
-		new_owner[i] = owner[i];
+		copy_owner[i] = owner[i];
 
-	/* Initialize dog */
-	dog->name = new_name;
-	dog->age = age;
-	dog->owner = new_owner;
+	new_dog->name = copy_name;
+	new_dog->age = age;
+	new_dog->owner = copy_owner;
 
-	return (dog);
+	return (new_dog);
 }
