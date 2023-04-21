86be3db5dfd09320cf1970354e422137f0d20420
Aya Berroukech <ayagym14@gmail.com>
2023-04-21 07:08:14 -0700

    constant and variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,16 +15,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
+	if (n == 0)
+		return;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
 		if (str == NULL)
-			printf("%s(nil)", separator == NULL ? "" : separator);
+			printf("(nil)");
 		else
-			printf("%s%s", separator == NULL ? "" : separator, str);
+			printf("%s", str);
+
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
 	}
+
 	va_end(args);
 
 	printf("\n");
