d7946fa17ce5b3e8b4aec5b5c815a3f00b74c0b1
Aya Berroukech <ayagym14@gmail.com>
2023-04-28 15:56:59 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}
+
+int main(void)
+{
+    print_before_main();
+
+    /* Rest of your program goes here */
+
+    return 0;
+}
