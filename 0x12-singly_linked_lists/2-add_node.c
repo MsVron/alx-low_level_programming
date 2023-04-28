072b04840a3916f3b226487f7f9089ab4f894e14
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-28 22:35:57 +0000

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,9 +7,9 @@
  *
  *Return: the address of the new element, or NULL if it failed
  */
-list_t* add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t * new_node;
+	list_t *new_node;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
