5f3b1b66d037bc15a1b1093c8180a47290022fd4
MsVron <126300537+MsVron@users.noreply.github.com>
2023-04-28 22:33:10 +0000

    lists lib

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,14 +14,13 @@
  *Description: singly linked list node structure
  *for ALX project
  */
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s * next;
-}
-
-list_t;
+} list_t;
 
 /*Function prototypes */
 size_t print_list(const list_t *h);
