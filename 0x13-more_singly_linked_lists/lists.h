9554ff9eea091b2256b5c2c9a3411bf3243a3906
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-04 02:06:06 +0100

    lists lib

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 
+
 /**
  *struct listint_s - singly linked list
  *@n: integer
@@ -20,6 +21,14 @@ typedef struct listint_s
 
 listint_t;
 
+typedef struct hash_table_s hash_table_t;
+
+typedef struct hash_node_s {
+    void *key;
+    void *value;
+    struct hash_node_s *next;
+} hash_node_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -35,5 +44,10 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+hash_table_t *hash_table_create(unsigned long int size, unsigned long int (*hash_func)(const char *key));
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif /*LISTS_H */
