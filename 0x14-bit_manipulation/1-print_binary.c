5c427261425a2aacdfcc1fef7857223441dea990
Aya Berroukech <ayagym14@gmail.com>
2023-05-04 15:32:01 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_binary - prints the binary representation of a number
+ *@n: the number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1 UL << ((sizeof(unsigned long int) *8) - 1);
+	int flag = 0;
+
+	while (mask > 0)
+	{
+		if ((n & mask) == mask)
+		{
+			flag = 1;
+			putchar('1');
+		}
+		else if (flag == 1 || mask == 1)
+		{
+			putchar('0');
+		}
+
+		mask = mask >> 1;
+	}
+}
