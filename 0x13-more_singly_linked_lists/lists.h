e9715ab1ff3807ad2d743cab5ae268d5dc773538
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-04 02:14:10 +0100

    lists lib

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+#include <string.h>
 
 /**
  *struct listint_s - singly linked list
@@ -23,11 +23,39 @@ listint_t;
 
 typedef struct hash_table_s hash_table_t;
 
-typedef struct hash_node_s {
-    void *key;
-    void *value;
-    struct hash_node_s *next;
-} hash_node_t;
+/**
+ *struct hash_table_s - Hash table data structure
+ *
+ *@size: The size of the array
+ *@array: An array of size @size
+ *Each cell of this array is a pointer to the first node of a linked list,
+ *because we want our HashTable to use a Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+}
+
+hash_table_t;
+
+/**
+ *struct hash_node_s - Node of a hash table
+ *
+ *@key: The key, string
+ *The key is unique in the HashTable
+ *@value: The value corresponding to a key
+ *@next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+}
+
+hash_node_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -39,10 +67,12 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+
 listint_t *find_listint_loop(listint_t *head);
 hash_table_t *hash_table_create(unsigned long int size, unsigned long int (*hash_func)(const char *key));
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
