a99dc25fea76cf8e5892f0986c7470f8e53d4fcf
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-05 00:01:51 +0100

    11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  *set_bit - Sets the value of a bit to 1 at a given index.
  *@n: A pointer to the number to modify.
@@ -16,3 +18,4 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	return (1);
 }
+=
