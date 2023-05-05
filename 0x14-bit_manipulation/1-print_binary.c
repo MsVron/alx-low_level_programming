21b22a739cbfbdd1178ad307bbc0d6d835bd7320
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-05 12:26:22 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,7 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << ((sizeof(unsigned long int) *8) - 1);
+	unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
 	int flag = 0;
 
 	while (mask > 0)
