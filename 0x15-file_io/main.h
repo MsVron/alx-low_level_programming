84c3040f4cd4320a715b1e0f808a957ca4fdb9eb
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-08 21:44:08 +0100

    main lib

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <elf.h>
 #include <stdint.h>
+#include <errno.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
