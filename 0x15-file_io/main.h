19ca9a33fee6263fd406754ec229f870f3a54f8a
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-08 21:23:19 +0100

    main lib

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -20,21 +20,22 @@ int append_text_to_file(const char *filename, char *text_content);
 void print_error(const char *message);
 void print_elf_header(const char *filename);
 
-typedef struct {
-    unsigned char e_ident[ELF_MAGIC_SIZE];
-    uint16_t e_type;
-    uint16_t e_machine;
-    uint32_t e_version;
-    uint64_t e_entry;
-    uint64_t e_phoff;
-    uint64_t e_shoff;
-    uint32_t e_flags;
-    uint16_t e_ehsize;
-    uint16_t e_phentsize;
-    uint16_t e_phnum;
-    uint16_t e_shentsize;
-    uint16_t e_shnum;
-    uint16_t e_shstrndx;
+typedef struct
+{
+	unsigned char e_ident[ELF_MAGIC_SIZE];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint64_t e_entry;
+	uint64_t e_phoff;
+	uint64_t e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
 } Elf64_Ehdr;
 
 
