715e19f781bb74b52774cfcef8537782c1ee9d0e
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-08 12:00:13 +0100

    main lib

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,6 +15,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 void print_error(const char *msg);
-cp(const char *file_from, const char *file_to);
+int cp(const char *file_from, const char *file_to);
 
 #endif /* MAIN_H */
