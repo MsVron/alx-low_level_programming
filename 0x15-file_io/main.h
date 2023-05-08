cbd2ec4a4f60df1e44f2b59c84e20ffebecd3701
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-08 21:22:26 +0100

    main lib

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #define BUFFER_SIZE 1024
+#define ELF_MAGIC_SIZE 4
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,15 +11,31 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <elf.h>
+#include <stdint.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-void print_error(const char *msg);
-int cp(const char *file_from, const char *file_to);
+void print_error(const char *message);
+void print_elf_header(const char *filename);
 
-void display_elf_header_info(Elf32_Ehdr *header);
+typedef struct {
+    unsigned char e_ident[ELF_MAGIC_SIZE];
+    uint16_t e_type;
+    uint16_t e_machine;
+    uint32_t e_version;
+    uint64_t e_entry;
+    uint64_t e_phoff;
+    uint64_t e_shoff;
+    uint32_t e_flags;
+    uint16_t e_ehsize;
+    uint16_t e_phentsize;
+    uint16_t e_phnum;
+    uint16_t e_shentsize;
+    uint16_t e_shnum;
+    uint16_t e_shstrndx;
+} Elf64_Ehdr;
 
 
 #endif /* MAIN_H */
