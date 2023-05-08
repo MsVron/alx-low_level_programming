26c5f1293a6de5738cfab0babdc8b8cbdc7ae6b1
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-08 12:13:33 +0100

    main lib

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -16,5 +17,8 @@ int append_text_to_file(const char *filename, char *text_content);
 
 void print_error(const char *msg);
 int cp(const char *file_from, const char *file_to);
+
+void display_elf_header_info(Elf32_Ehdr *header);
+
 
 #endif /* MAIN_H */
