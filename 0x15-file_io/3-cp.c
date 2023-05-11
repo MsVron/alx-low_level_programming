9a4023503859b1221bbab5408049a0a2ee154578
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-11 22:25:45 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -45,37 +45,6 @@ int open_dest_file(const char *file_to)
 }
 
 /**
- *copy_file_contents - Copies the contents of the source file to the destination file.
- *@fd_from: The file descriptor of the source file.
- *@fd_to: The file descriptor of the destination file.
- *
- *Return: 0 on success, or the appropriate exit code on failure.
- */
-int copy_file_contents(int fd_from, int fd_to)
-{
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to destination file\n");
-			return (99);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from source file\n");
-		return (98);
-	}
-
-	return (0);
-}
-
-/**
  *cp - Copies the content of a file to another file.
  *@file_from: The source file.
  *@file_to: The destination file.
