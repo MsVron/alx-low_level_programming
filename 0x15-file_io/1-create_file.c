a0b1709199397e5310bc39447206cabf05e899de
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-11 22:00:35 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,6 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		int bytes_written = write(file_descriptor, text_content, strlen(text_content));
+		
 		if (bytes_written == -1)
 		{
 			close(file_descriptor);
@@ -33,5 +34,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(file_descriptor);
+
 	return (1);
 }
