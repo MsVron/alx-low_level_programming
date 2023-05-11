8089e8fda33b40dcf34a69f09b02ccf6b8272918
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-11 22:29:21 +0100

    main lib

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,6 +18,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copy_file_contents(int fd_from, int fd_to);
 
 void print_error(const char *message);
 void print_elf_header(const char *filename);
