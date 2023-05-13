592e4121bfa91abae48777215d077517b026a398
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-13 12:54:06 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,125 +1,100 @@
 #include "main.h"
-#include <stdarg.h>
 
 /**
- * print_error - Prints an error message to the POSIX standard error.
- * @msg: The error message to print.
- * @...: Additional arguments for the format string.
- */
-void print_error(const char *msg, ...)
-{
-	va_list args;
-	va_start(args, msg);
-
-	vdprintf(STDERR_FILENO, msg, args);
-	va_end(args);
-}
-
-/**
- *copy_file - Copies the content of a file to another file.
- *@fd_from: The source file descriptor.
- *@fd_to: The destination file descriptor.
+ *file_exists - Checks if a file exists.
+ *@filename: The path to the file to check.
  *
- *Return: 0 on success, or the appropriate exit code on failure.
+ *Returns 1 if the file exists, or 0 if it does not.
  */
-int copy_file(int fd_from, int fd_to)
+int file_exists(const char *filename)
 {
-	int bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	FILE *file = fopen(filename, "r");
+	if (file == NULL)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			print_error("Error: Can't write to file descriptor %d\n", fd_to);
-			return (99);
-		}
+		return 0;
 	}
 
-	if (bytes_read == -1)
-	{
-		print_error("Error: Can't read from file descriptor %d\n", fd_from);
-		return (98);
-	}
-
-	return (0);
+	fclose(file);
+	return 1;
 }
 
 /**
- *cp - Copies the content of a file to another file.
+ *cp - Copy the contents from one file to another.
  *@file_from: The source file.
  *@file_to: The destination file.
  *
- *Return: 0 on success, or the appropriate exit code on failure.
+ *Returns 0 on success, or -1 on failure.
  */
 int cp(const char *file_from, const char *file_to)
 {
-	int fd_from, fd_to, ret;
-	struct stat st;
-
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
+	/*Check if the source file exists */
+	if (!file_exists(file_from))
 	{
-		print_error("Error: Can't read from file %s\n", file_from);
-		return (98);
+		fprintf(stderr, "Error: Source file %s does not exist\n", file_from);
+		return -1;
 	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
-		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fd_to == -1)
+	/*Open the files */
+	FILE *file_from_fd = fopen(file_from, "r");
+	if (file_from_fd == NULL)
 	{
-		print_error("Error: Can't write to %s\n", file_to);
-		close(fd_from);
-		return (99);
+		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
+		return -1;
 	}
 
-	ret = copy_file(fd_from, fd_to);
-
-	if (fstat(fd_from, &st) == 0)
+	FILE *file_to_fd = fopen(file_to, "w");
+	if (file_to_fd == NULL)
 	{
-		if (fchmod(fd_to, st.st_mode) == -1)
-		{
-			print_error("Error: Can't set file permissions on %s\n", file_to);
-			ret = 100;
-		}
-	}
-	else
-	{
-		print_error("Error: Can't get file permissions from %s\n", file_from);
-		ret = 100;
+		fprintf(stderr, "Error: Can't write to file %s\n", file_to);
+		fclose(file_from_fd);
+		return -1;
 	}
 
-	if (close(fd_from) == -1)
+	/*Copy contents from file_from to file_to */
+	char buffer[1024];
+	size_t bytes_read;
+	while ((bytes_read = fread(buffer, 1, sizeof(buffer), file_from_fd)) > 0)
 	{
-		print_error("Error: Can't close fd %d\n", fd_from);
-		close(fd_to);
-		return (100);
+		fwrite(buffer, 1, bytes_read, file_to_fd);
 	}
 
-	if (close(fd_to) == -1)
+	/*Close the files */
+	if (fclose(file_from_fd) != 0)
 	{
-		print_error("Error: Can't close fd %d\n", fd_to);
-		return (100);
+		fprintf(stderr, "Error: Can't close file %s\n", file_from);
+		return -1;
 	}
 
-	return (ret);
+	if (fclose(file_to_fd) != 0)
+	{
+		fprintf(stderr, "Error: Can't close file %s\n", file_to);
+		return -1;
+	}
+
+	return 0;
 }
 
 /**
  *main - Entry point of the program.
  *@argc: The number of command-line arguments.
- *@argv: An array containing the command-line arguments.
+ *@argv: An array of strings containing the command-line arguments.
  *
- *Return: 0 on success, or the appropriate exit code on failure.
+ *Return: 0 on success, or an error code on failure.
  */
 int main(int argc, char *argv[])
 {
+	/*Check the number of arguments */
 	if (argc != 3)
 	{
-		print_error("Usage: cp file_from file_to");
-		return (97);
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		return 97;
 	}
 
-	exit(cp(argv[1], argv[2]));
+	/*Copy the file */
+	if (cp(argv[1], argv[2]) < 0)
+	{
+		return 99;
+	}
+
+	return 0;
 }
