6504450dd7d41bd6035bde564292013f9712d3a0
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-13 01:04:57 +0100

    ELF

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,63 +1,51 @@
 #include "main.h"
-#define ELF_MAGIC 0x7f454c46
 
-/*
- *main()
- *
- *The main() function is the entry point for the program. It parses the command-line arguments, opens the ELF file, reads the ELF header, and prints the ELF header information.
- *
- *Parameters:
- *
- *argc: The number of command-line arguments.
- *argv: An array of pointers to the command-line arguments.
- */
+void print_error(const char *message)
+{
+	fprintf(stderr, "Error: %s\n", message);
+	exit(98);
+}
+
+void print_elf_header(const char *filename)
+{
+	int fd = open(filename, O_RDONLY);
+	CustomElf64_Ehdr elf_header;
+	ssize_t bytes_read = read(fd, &elf_header, sizeof(CustomElf64_Ehdr));
+	if (fd == -1)
+		print_error(strerror(errno));
+
+	if (bytes_read == -1)
+		print_error(strerror(errno));
+
+	if (bytes_read != sizeof(CustomElf64_Ehdr))
+		print_error("Invalid ELF header");
+
+	/*Check ELF magic */
+	if (memcmp(elf_header.e_ident, "\x7f"
+			"ELF", ELF_MAGIC_SIZE) != 0)
+		print_error("Not an ELF file");
+
+	printf("Magic:   %c%c%c\n", elf_header.e_ident[1], elf_header.e_ident[2], elf_header.e_ident[3]);
+	printf("Class:   %d-bit\n", elf_header.e_ident[4] == 1 ? 32 : 64);
+	printf("Data:    %s\n", elf_header.e_ident[5] == 1 ? "Little endian" : "Big endian");
+	printf("Version: %d\n", elf_header.e_ident[6]);
+	printf("OS/ABI:  %d\n", elf_header.e_ident[7]);
+	printf("ABI Version: %d\n", elf_header.e_ident[8]);
+	printf("Type:    %d\n", elf_header.e_type);
+	printf("Entry point address: 0x%lx\n", elf_header.e_entry);
+
+	close(fd);
+}
+
 int main(int argc, char *argv[])
 {
-	/*Check the number of command-line arguments. */
 	if (argc != 2)
 	{
-		/*Print an error message and exit. */
-		fprintf(stderr, "Usage: elf_header elf_filename\n");
-		exit(1);
+		fprintf(stderr, "Usage: %s<elf_filename>\n", argv[0]);
+		return 1;
 	}
 
-	/*Open the ELF file. */
-	FILE * fp;
-	fp = fopen(argv[1], "rb");
-	if (fp == NULL)
-	{
-		/*Print an error message and exit. */
-		perror("fopen");
-		exit(1);
-	}
+	print_elf_header(argv[1]);
 
-	unsigned char elf_header[64];
-	if (fread(elf_header, 1, 64, fp) != 64)
-	{
-		/*error */
-		perror("fread");
-		exit(1);
-	}
-
-	if (elf_header[0] != ELF_MAGIC)
-	{
-		/*error msg */
-		fprintf(stderr, "Not an ELF file\n");
-		exit(1);
-	}
-
-	/*ELF header information. */
-	printf("ELF Header:\n");
-	printf("  Magic:   0x%08x\n", elf_header[0] << 24 | elf_header[1] << 16 | elf_header[2] << 8 | elf_header[3]);
-	printf("  Class:                             %d\n", elf_header[4] >> 4);
-	printf("  Data:                              %d\n", elf_header[4] &0xf);
-	printf("  Version:                           %d\n", elf_header[5]);
-	printf("  OS/ABI:                            %d\n", elf_header[6] >> 4);
-	printf("  ABI Version:                       %d\n", elf_header[6] &0xf);
-	printf("  Type:                              %d\n", elf_header[7]);
-	printf("  Entry point address:               0x%08x\n", elf_header[8] << 24 | elf_header[9] << 16 | elf_header[10] << 8 | elf_header[11]);
-
-	fclose(fp);
-
-	return (0);
+	return 0;
 }
