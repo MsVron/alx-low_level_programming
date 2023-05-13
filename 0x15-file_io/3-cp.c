028489d426c96023eee43feb3d91c76a211dfa62
MsVron <126300537+MsVron@users.noreply.github.com>
2023-05-13 12:57:17 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,9 @@
 #include "main.h"
-
-/**
- *file_exists - Checks if a file exists.
- *@filename: The path to the file to check.
- *
- *Returns 1 if the file exists, or 0 if it does not.
- */
-int file_exists(const char *filename)
-{
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
-	{
-		return 0;
-	}
-
-	fclose(file);
-	return 1;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 1024
 
 /**
  *cp - Copy the contents from one file to another.
@@ -27,74 +14,77 @@ int file_exists(const char *filename)
  */
 int cp(const char *file_from, const char *file_to)
 {
-	/*Check if the source file exists */
-	if (!file_exists(file_from))
+	int fd_from, fd_to, ret;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	/*Open the source file */
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
-		fprintf(stderr, "Error: Source file %s does not exist\n", file_from);
-		return -1;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		return 98;
 	}
 
-	/*Open the files */
-	FILE *file_from_fd = fopen(file_from, "r");
-	if (file_from_fd == NULL)
+	/*Open the destination file */
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
-		return -1;
-	}
-
-	FILE *file_to_fd = fopen(file_to, "w");
-	if (file_to_fd == NULL)
-	{
-		fprintf(stderr, "Error: Can't write to file %s\n", file_to);
-		fclose(file_from_fd);
-		return -1;
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		close(fd_from);
+		return 99;
 	}
 
 	/*Copy contents from file_from to file_to */
-	char buffer[1024];
-	size_t bytes_read;
-	while ((bytes_read = fread(buffer, 1, sizeof(buffer), file_from_fd)) > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		fwrite(buffer, 1, bytes_read, file_to_fd);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			close(fd_from);
+			close(fd_to);
+			return 99;
+		}
+	}
+
+	/*Check for read error */
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		return 98;
 	}
 
 	/*Close the files */
-	if (fclose(file_from_fd) != 0)
+	ret = close(fd_from);
+	if (ret == -1)
 	{
-		fprintf(stderr, "Error: Can't close file %s\n", file_from);
-		return -1;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		return 100;
 	}
 
-	if (fclose(file_to_fd) != 0)
+	ret = close(fd_to);
+	if (ret == -1)
 	{
-		fprintf(stderr, "Error: Can't close file %s\n", file_to);
-		return -1;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		return 100;
 	}
 
 	return 0;
 }
 
-/**
- *main - Entry point of the program.
- *@argc: The number of command-line arguments.
- *@argv: An array of strings containing the command-line arguments.
- *
- *Return: 0 on success, or an error code on failure.
- */
 int main(int argc, char *argv[])
 {
-	/*Check the number of arguments */
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return 97;
 	}
 
-	/*Copy the file */
-	if (cp(argv[1], argv[2]) < 0)
-	{
+	if (cp(argv[1], argv[2]) != 0)
 		return 99;
-	}
 
 	return 0;
 }
