b4a8b33bb67da727fc37e936dd3605abf5ff0553
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-16 11:42:35 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,17 +7,17 @@
  *@n: Value to be stored in the new node
  *Return: Address of the new node, or NULL if it failed
  */
-dlistint_t* insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *current;
 	unsigned int i;
 
 	if (h == NULL)
-		return NULL;
+		return (NULL);
 
 	/*Special case: inserting at the beginning */
 	if (idx == 0)
-		return add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 
 	/*Traverse the list to the desired position */
 	current = *h;
@@ -26,12 +26,12 @@ dlistint_t* insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	/*Check if it's not possible to add the new node */
 	if (current == NULL)
-		return NULL;
+		return (NULL);
 
 	/*Create the new node and assign its values */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 	new_node->n = n;
 
 	/*Adjust the links of the surrounding nodes */
@@ -41,5 +41,5 @@ dlistint_t* insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current->next = new_node;
 	new_node->prev = current;
 
-	return new_node;
+	return (new_node);
 }
