2bc3df6d5e656dd11743d8731ac8052a7e5b0a9e
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-21 22:57:58 +0100

    Initial commit

---
+++ 0x18-dynamic_libraries/memcpy.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    char *originalDest = dest;
+    while (n > 0) {
+        *dest = *src;
+        dest++;
+        src++;
+        n--;
+    }
+    return originalDest;
+}
