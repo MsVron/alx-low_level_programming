2a5daee201ef343d1ffce25dec5b89045e51020d
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-21 23:00:40 +0100

    Initial commit

---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+int _strlen(char *s) {
+    int length = 0;
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
+    return length;
+}
