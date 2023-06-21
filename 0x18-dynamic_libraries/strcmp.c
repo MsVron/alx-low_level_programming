7dac6e4b1bda890b0cb857252a52dcce68b9ca51
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-21 23:00:07 +0100

    Initial commit

---
+++ 0x18-dynamic_libraries/strcmp.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+int _strcmp(char *s1, char *s2) {
+    while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
