ecdaad51ef5bf14a412a81bf50c3a7f945e7b74d
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-21 22:50:28 +0100

    Initial commit

--- 0x18-dynamic_libraries/dynamic.c
+++ 0x18-dynamic_libraries/dynamic.c
@@ -57,13 +57,20 @@ char *_strcpy(char *dest, char *src) {
     return originalDest;
 }
 
-int _atoi(char *s) {
+int _atoi(void) {
+    char input[100];
+    printf("Enter a number: ");
+    scanf("%s", input);
+
     int result = 0;
     int sign = 1;
+    char *s = input;
+
     if (*s == '-') {
         sign = -1;
         s++;
     }
+
     while (*s != '\0') {
         if (*s >= '0' && *s <= '9') {
             result = result * 10 + (*s - '0');
@@ -72,6 +79,7 @@ int _atoi(char *s) {
         }
         s++;
     }
+
     return result * sign;
 }
 
