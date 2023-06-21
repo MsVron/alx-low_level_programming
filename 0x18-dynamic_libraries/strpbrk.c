59234fb4faab3490f9e52c4d01e092da7e2a2227
MsVron <126300537+MsVron@users.noreply.github.com>
2023-06-21 23:09:31 +0100

    Initial commit

---
+++ 0x18-dynamic_libraries/strpbrk.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept) {
+    while (*s != '\0') {
+        char *temp = accept;
+        while (*temp != '\0') {
+            if (*s == *temp) {
+                return s;
+            }
+            temp++;
+        }
+        s++;
+    }
+    return NULL;
+}
