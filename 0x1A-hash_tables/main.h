30c7438c1bad7b48abfc9e08b8ebcdd65573d3da
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-06 12:22:58 +0100

    main lib

--- 0x1A-hash_tables/main.h
+++ 0x1A-hash_tables/main.h
@@ -1,7 +1,37 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype */
+/* Required headers */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Struct definitions */
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key string
+ * @value: The value corresponding to the key
+ * @next: Pointer to the next node in case of collisions
+ */
+typedef struct hash_node_s
+{
+    char *key;
+    char *value;
+    struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: The size of the hash table array
+ * @array: Array of pointers to the hash nodes
+ */
+typedef struct hash_table_s
+{
+    unsigned long int size;
+    hash_node_t **array;
+} hash_table_t;
+
+/* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif /* MAIN_H */
